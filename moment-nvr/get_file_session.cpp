7dee1829a451281c9c2bb8408dbafc714387d1b2
Dmitry Shatrov <shatrov@gmail.com>
2013-04-02 17:31:18 +0200

    GetFileSession initial implementation

--- moment-nvr/get_file_session.cpp
+++ moment-nvr/get_file_session.cpp
@@ -6,6 +6,125 @@ using namespace Moment;
 
 namespace MomentNvr {
 
+Mp4Muxer::Frontend const GetFileSession::mp4_muxer_frontend = {
+    muxerStateChanged
+};
+
+void
+GetFileSession::muxerStateChanged (Mp4Muxer::MuxState   const mux_state,
+                                   void               * const _self)
+{
+    logD_ (_func_);
+}
+
+MediaReader::ReadFrameBackend const GetFileSession::read_frame_backend = {
+    audioFrame,
+    videoFrame
+};
+
+mt_sync_domain (readTask) MediaReader::ReadFrameResult
+GetFileSession::audioFrame (VideoStream::AudioMessage * const mt_nonnull msg,
+                            void                      * const _self)
+{
+    GetFileSession * const self = static_cast <GetFileSession*> (_self);
+    assert (msg->prechunk_size == 0);
+
+    logD_ (_func, " ts ", msg->timestamp_nanosec, " ", msg->frame_type);
+
+    if (self->session_state == SessionState_PassOne) {
+        self->mp4_muxer.pass1_frame (Mp4Muxer::FrameType_Audio,
+                                     msg->timestamp_nanosec,
+                                     msg->msg_len,
+                                     false /* is_sync_sample */);
+    } else {
+        self->mp4_muxer.pass2_frame (Mp4Muxer::FrameType_Audio,
+                                     msg->timestamp_nanosec,
+                                     msg->page_list.first,
+                                     msg->msg_offset,
+                                     msg->msg_len);
+    }
+
+    return MediaReader::ReadFrameResult_Success;
+}
+
+mt_sync_domain (readTask) MediaReader::ReadFrameResult
+GetFileSession::videoFrame (VideoStream::VideoMessage * const mt_nonnull msg,
+                            void                      * const _self)
+{
+    GetFileSession * const self = static_cast <GetFileSession*> (_self);
+    assert (msg->prechunk_size == 0);
+
+    logD_ (_func, (self->session_state == SessionState_PassOne ? "ONE" : "TWO"), " ts ", msg->timestamp_nanosec, " ", msg->frame_type, " len ", msg->msg_len);
+
+    if (self->session_state == SessionState_PassOne) {
+        if (msg->frame_type == VideoStream::VideoFrameType::AvcSequenceHeader) {
+            self->mp4_muxer.pass1_avcSequenceHeader (msg->page_list.first,
+                                                     msg->msg_offset,
+                                                     msg->msg_len);
+        } else
+        if (msg->frame_type.isVideoData()) {
+            self->mp4_muxer.pass1_frame (Mp4Muxer::FrameType_Video,
+                                         msg->timestamp_nanosec,
+                                         msg->msg_len,
+                                         msg->frame_type.isKeyFrame());
+        }
+    } else {
+        if (msg->frame_type.isVideoData()) {
+            self->mp4_muxer.pass2_frame (Mp4Muxer::FrameType_Video,
+                                         msg->timestamp_nanosec,
+                                         msg->page_list.first,
+                                         msg->msg_offset,
+                                         msg->msg_len);
+        }
+    }
+
+    return MediaReader::ReadFrameResult_Success;
+}
+
+bool
+GetFileSession::readTask (void * const _self)
+{
+    GetFileSession * const self = static_cast <GetFileSession*> (_self);
+
+    logD_ (_func_);
+
+    // TEST
+    for (unsigned i = 0; i < 2; ++i) {
+        for (;;) {
+            logD_ (_func, "calling readMoreData()");
+            MediaReader::ReadFrameResult const res = self->media_reader.readMoreData (&read_frame_backend, self);
+
+            if (res == MediaReader::ReadFrameResult_Failure) {
+                logE_ (_func, "ReadFrameResult_Failure");
+                return false /* do not reschedule */;
+            }
+
+            if (res == MediaReader::ReadFrameResult_BurstLimit) {
+                logD_ (_func, "ReadFrameResult_BurstLimit");
+                return false /* do not reschedule */;
+            }
+
+            if (res == MediaReader::ReadFrameResult_NoData) {
+                // TEST
+                if (i == 0)
+                    self->mp4_muxer.pass1_complete ();
+
+                logD_ (_func, "ReadFrameResult_NoData");
+                break;
+            }
+        }
+
+        if (self->session_state == SessionState_PassOne) {
+            self->session_state = SessionState_PassTwo;
+            self->media_reader.reset ();
+        } else {
+            break;
+        }
+    }
+
+    return false /* do not reschedule */;
+}
+
 Sender::Frontend const GetFileSession::sender_frontend = {
     senderStateChanged,
     senderClosed
@@ -16,6 +135,8 @@ GetFileSession::senderStateChanged (Sender::SendState   const send_state,
                                     void              * const _self)
 {
     GetFileSession * const self = static_cast <GetFileSession*> (_self);
+
+    // TODO Check 'started'
 }
 
 void
@@ -23,20 +144,74 @@ GetFileSession::senderClosed (Exception * const exc,
                               void      * const _self)
 {
     GetFileSession * const self = static_cast <GetFileSession*> (_self);
+
+    // TODO Check 'started'
+}
+
+void
+GetFileSession::start ()
+{
+    mutex.lock ();
+    started = true;
+    mutex.unlock ();
+
+    deferred_reg.scheduleTask (&read_task, false /* permanent */);
 }
 
 mt_const void
-GetFileSession::init (HttpRequest * const mt_nonnull req,
-                      Sender      * const mt_nonnull sender,
-                      ConstMemory   const stream_name,
-                      Time          const start_unixtime_sec,
-                      Time          const duration_sec,
+GetFileSession::init (MomentServer * const mt_nonnull moment,
+                      HttpRequest  * const mt_nonnull req,
+                      Sender       * const mt_nonnull sender,
+                      PagePool     * const page_pool,
+                      Vfs          * const vfs,
+                      ConstMemory    const stream_name,
+                      Time           const start_unixtime_sec,
+                      Time           const duration_sec,
                       CbDesc<Frontend> const &frontend)
 {
+    this->moment = moment;
+    this->sender = sender;
     this->frontend = frontend;
+    this->duration_sec = duration_sec;
+
+    media_reader.init (page_pool, vfs, stream_name, /* TEST start_unixtime_sec */ getUnixtime() - 10);
+    mp4_muxer.init (page_pool, sender, CbDesc<Mp4Muxer::Frontend> (&mp4_muxer_frontend, this, this));
+
+    thread_ctx = moment->getReaderThreadPool()->grabThreadContext (stream_name);
+    if (thread_ctx) {
+        reader_thread_ctx = thread_ctx;
+    } else {
+        logE_ (_func, "Could not get reader thread context: ", exc->toString());
+        reader_thread_ctx = NULL;
+        thread_ctx = moment->getServerApp()->getServerContext()->getMainThreadContext();
+    }
+
+    deferred_reg.setDeferredProcessor (thread_ctx->getDeferredProcessor());
 
     sender->getEventInformer()->subscribe (
             CbDesc<Sender::Frontend> (&sender_frontend, this, this));
+}
+
+GetFileSession::GetFileSession ()
+    : sender            (this /* coderef_container */),
+      thread_ctx        (this /* coderef_container */),
+      reader_thread_ctx (NULL),
+      media_reader      (this /* coderef_container */),
+      mp4_muxer         (this /* coderef_container */),
+      session_state     (SessionState_PassOne),
+      started           (false)
+{
+    read_task.cb = CbDesc<DeferredProcessor::TaskCallback> (readTask, this, this);
+}
+
+GetFileSession::~GetFileSession ()
+{
+    deferred_reg.release ();
+
+    if (reader_thread_ctx) {
+        moment->getReaderThreadPool()->releaseThreadContext (reader_thread_ctx);
+        reader_thread_ctx = NULL;
+    }
 }
 
 }
--- moment-nvr/get_file_session.h
+++ moment-nvr/get_file_session.h
@@ -4,6 +4,8 @@
 
 #include <moment/libmoment.h>
 
+#include <moment-nvr/media_reader.h>
+
 
 namespace MomentNvr {
 
@@ -22,10 +24,50 @@ public:
     };
 
 private:
+    enum SessionState {
+        SessionState_PassOne,
+        SessionState_PassTwo
+    };
+
+    mt_const Ref<MomentServer> moment;
     mt_const DataDepRef<Sender> sender;
     mt_const Cb<Frontend> frontend;
 
-    Mp4Muxer mp4_muxer;
+    mt_const Time duration_sec;
+
+    mt_const DataDepRef<ServerThreadContext> thread_ctx;
+    mt_const ServerThreadContext *reader_thread_ctx;
+
+    MediaReader media_reader;
+    mt_sync_domain (readTask) Mp4Muxer mp4_muxer;
+
+    mt_sync_domain (readTask) SessionState session_state;
+
+    DeferredProcessor::Task read_task;
+    DeferredProcessor::Registration deferred_reg;
+
+    mt_mutex (mutex) bool started;
+
+  mt_iface (Mp4Muxer::Frontend)
+    static Mp4Muxer::Frontend const mp4_muxer_frontend;
+
+    static void muxerStateChanged (Mp4Muxer::MuxState  mux_state,
+                                   void               *_self);
+  mt_iface_end
+
+  mt_iface (MediaReader::ReadFrameBackend)
+    static MediaReader::ReadFrameBackend const read_frame_backend;
+
+    mt_sync_domain (readTask) static
+            MediaReader::ReadFrameResult audioFrame (VideoStream::AudioMessage * mt_nonnull msg,
+                                                     void                      *_self);
+
+    mt_sync_domain (readTask) static
+            MediaReader::ReadFrameResult videoFrame (VideoStream::VideoMessage * mt_nonnull msg,
+                                                     void                      *_self);
+  mt_iface_end
+
+    static bool readTask (void * const _self);
 
   mt_iface (Sender::Frontend)
     static Sender::Frontend const sender_frontend;
@@ -38,17 +80,20 @@ private:
   mt_iface_end 
 
 public:
-    mt_const void init (HttpRequest * const mt_nonnull req,
-                        Sender      * const mt_nonnull sender,
-                        ConstMemory   const stream_name,
-                        Time          const start_unixtime_sec,
-                        Time          const duration_sec,
+    void start ();
+
+    mt_const void init (MomentServer * const mt_nonnull moment,
+                        HttpRequest  * const mt_nonnull req,
+                        Sender       * const mt_nonnull sender,
+                        PagePool     * const page_pool,
+                        Vfs          * const vfs,
+                        ConstMemory    const stream_name,
+                        Time           const start_unixtime_sec,
+                        Time           const duration_sec,
                         CbDesc<Frontend> const &frontend);
 
-    GetFileSession ()
-        : sender    (this /* coderef_container */),
-          mp4_muxer (this /* coderef_container */)
-    {}
+     GetFileSession ();
+    ~GetFileSession ();
 };
 
 }
